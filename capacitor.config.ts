5a71599e73c51ba4a872f5a8bbf8ed13eb6ca960
hdhsgdysud-png <hdhsgdysud@gmail.com>
2026-04-28 21:00:45 +0300

    V1.7

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.spicy.falcon',
+  appId: 'com.amr.falcon',
   appName: 'FALCON',
   webDir: 'public'
 };
