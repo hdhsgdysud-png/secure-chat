5f74ed18e1ced53ec52dd4381a5e6c2b261eb2ae
hdhsgdysud-png <hdhsgdysud@gmail.com>
2026-04-30 00:43:11 +0300

    V3.5

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.spicy.falcon',
+  appId: 'com.scc.falcon',
   appName: 'FALCON',
   webDir: 'public',
   server: {
